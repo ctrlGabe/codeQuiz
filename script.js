ff5534a06d3d0a525724fd195aac642152dcaa1d
Gabe <gabriel47rivera@gmail.com>
2020-09-28 13:09:00 -0400

    added start button and answer buttons in html

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 var questions = [
   {
     question: "What is javascript?",
-    choices: ["hippo", "12", "code", "purple", "bananas"],
+    choices: ["hippo", "12", "code", "purple"],
     correctAnswer: 2,
   },
   {
     question: "What is html?",
-    choices: ["hippo", "12", "code", "purple", "bananas"],
+    choices: ["hippo", "12", "code", "purple"],
     correctAnswer: 2,
   },
   {
     question: "What is css?",
-    choices: ["hippo", "12", "code", "purple", "bananas"],
+    choices: ["hippo", "12", "code", "purple"],
     correctAnswer: 2,
   },
   {
     question: "What is jquery?",
-    choices: ["hippo", "12", "code", "purple", "bananas"],
+    choices: ["hippo", "12", "code", "purple"],
     correctAnswer: 2,
   },
 ];
-\;
-var selections = [];
 
-var answer = document.getElementById("#answer");
-var question = document.getElementById("question");
+var startButton = document.getElementById("start");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("Boom baby");
+}
+
+function nextQuestion() {}
+
+function pickAnswer() {}
