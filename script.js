d9670debda6929d4ae7fc4c6a2d445e7b666286d
Gabe <gabriel47rivera@gmail.com>
2020-09-28 11:12:59 -0400

    added a card to html page and questions to js file

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    question: "What is javascript?",
+    choices: ["hippo", "12", "code", "purple", "bananas"],
+    correctAnswer: 2,
+  },
+  {
+    question: "What is html?",
+    choices: ["hippo", "12", "code", "purple", "bananas"],
+    correctAnswer: 2,
+  },
+  {
+    question: "What is css?",
+    choices: ["hippo", "12", "code", "purple", "bananas"],
+    correctAnswer: 2,
+  },
+  {
+    question: "What is jquery?",
+    choices: ["hippo", "12", "code", "purple", "bananas"],
+    correctAnswer: 2,
+  },
+];
+\;
+var selections = [];
+
+var answer = document.getElementById("#answer");
+var question = document.getElementById("question");
