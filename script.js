78675a08ec2b4910c812432e40106da6a36bc65e
Gabe <gabriel47rivera@gmail.com>
2020-09-30 18:37:15 -0400

    finally got the questions and answers to display to the page

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-var questionChoices = [
+var questions = [
   {
-    question: "What is javascript?",
-    choices: ["hippo", "12", "code", "purple"],
-    correctAnswer: 2,
+    question: "Who created Javascript?",
+    answers: ["Arnold Schwarzenegger", "Brendan Eich", "Bill Gates", "Al Gore"],
+    correct: "Brendan Eich",
   },
   {
-    question: "What is html?",
-    choices: ["hippo", "12", "code", "purple"],
-    correctAnswer: 2,
+    question: "Inside which HTML element do we write Javascript?",
+    answers: ["<body>", "<scripting>", "<script>", "<javascript>"],
+    correct: "<script>",
   },
   {
-    question: "What is css?",
-    choices: ["hippo", "12", "code", "purple"],
-    correctAnswer: 2,
+    question: "How do you create a function in Javascript?",
+    answers: [
+      "function = myFunction()",
+      "myFunction() = function",
+      "function:myFunction()",
+      "function myFunction()",
+    ],
+    correct: "function myFunction()",
   },
   {
-    question: "What is jquery?",
-    choices: ["hippo", "12", "code", "purple"],
-    correctAnswer: 2,
+    question: "Which symbol is used to wrap an array?",
+    answers: ["[]", "()", "{}", "//"],
+    correct: "[]",
+  },
+  {
+    question: "How do you declare a Javascript variable?",
+    answers: [
+      "variable = myName;",
+      " v myName;",
+      "var myName;",
+      "variable myName;",
+    ],
+    correct: "var myName;",
   },
 ];
 
@@ -29,26 +44,80 @@ var questionEl = document.getElementById("question");
 
 var answerEl = document.getElementById("answer");
 
+var quizContainer = document.getElementById("quiz");
+
+var questionCount = 0;
+
 startButton.addEventListener("click", startGame);
 
-quizContainer = document.getElementById("quiz");
-
 function startGame() {
-  console.log("Boom baby");
+  console.log("Start game works");
   startButton.classList.add("hide");
   preQuiz.classList.add("hide");
   quizContainer.classList.remove("hide");
-  questionCount = 0;
-  nextQuestion();
+  // startTimer()
+  startQuiz();
 }
 
-function nextQuestion() {
-  showQuestion(questionCount);
+function startQuiz() {
+  console.log("start quiz works");
+  questionCount = 0;
+  showQuestion(questions[questionCount]);
 }
+
+questions[questionCount].answers.textContent
+
 function showQuestion(question) {
-  questionEl.textContent = questionChoices.question;
+  console.log("show question works");
+  reset();
+  questionEl.textContent = question.question;
+
+  for (let i = 0; i < question.answers.length; i++) {
+    var help = question.answers[i];
+    var button = document.createElement("button");
+    button.innerText = help.innerText;
+    button.classList.add("btn");
+    answerEl.appendChild(button);
+  }
+
+  // question.answers.forEach(function () {
+  //   var button = document.createElement("button");
+  //   button.innerText = question.answers;
+  //   console.log(question.answers);
+  //   button.classList.add("btn");
+  //   // if (answer.correct) {
+  //   //   button.dataset.correct = answer.correct;
+  //   // }
+  //   button.addEventListener("click", pickAnswer);
+  //   answerEl.appendChild(button);
+  // });
+}
+
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  document.getElementById("btn-1").textContent =
+    questions[questionCount].answers[0];
+  document.getElementById("btn-2").textContent =
+    questions[questionCount].answers[1];
+  document.getElementById("btn-3").textContent =
+    questions[questionCount].answers[2];
+  document.getElementById("btn-4").textContent =
+    questions[questionCount].answers[3];
+}
+document.getElementById("btn-1").addEventListener("click", pickAnswer);
+document.getElementById("btn-2").addEventListener("click", pickAnswer);
+document.getElementById("btn-3").addEventListener("click", pickAnswer);
+document.getElementById("btn-4").addEventListener("click", pickAnswer);
+
+function reset() {
+  while (answerEl.firstChild) {
+    answerEl.removeChild(answerEl.firstChild);
+  }
 }
 
 function pickAnswer() {
   questionCount++;
+  showQuestion(questions[questionCount]);
 }
+
+// justin
